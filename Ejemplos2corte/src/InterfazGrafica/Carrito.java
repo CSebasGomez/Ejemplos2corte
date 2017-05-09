5ced5c73d7270d99b776f86d91fc9a853e55d878
Estudiante <Estudiante@168.176.37.116>
2017-05-08 20:59:34 -0500

    Ejemplos clase 4

--- Ejemplos2corte/src/InterfazGrafica/Carrito.java
+++ Ejemplos2corte/src/InterfazGrafica/Carrito.java
@@ -7,12 +7,14 @@ package InterfazGrafica;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -41,63 +43,117 @@ class NewPanel2 extends JPanel implements ActionListener,MouseListener{
     private int x=20;
     private int y=8;
     private Timer timer;
+    private int secuencia;
     int z=0;
+    
+    public Image loadImage(String Fondo){
+        ImageIcon fondito = new ImageIcon(Fondo);
+       
+        Image image = fondito.getImage();
+        return image;
+    }
      @Override
      
     public void actionPerformed(ActionEvent e) {
             
             if(z%2==0){
-                x+=20;
-                y+=5;
+                x+=10;
+                y+=2;
+                if(this.secuencia ==3){
+                this.secuencia=0;
+                }
+            else{
+                this.secuencia++;
+                }
                 repaint();
+                
                 if(x==1000){
                     z++;
                 }
             }
             else{
-                x-=20;
-                y-=5;
+                x-=1;
+                y-=2;
+                if(this.secuencia ==5){
+                this.secuencia=0;
+                }
+                else{
+                this.secuencia++;
+                }
+                repaint();
                 repaint();
                 if(x==0){
                     z++;
                 }
             }
+
+//        if(checkCollisions()){
+//            x+=0;
+//            y-=1;
+//        }
+//        else{
+//            x+=1;
+//            y+=1;
+//        }
+//        x+=5;
+//        y+=1;
+//        if(this.secuencia ==5){
+//        this.secuencia=0;
+//        }
+//        else{
+//            this.secuencia++;
+//        }
+//        repaint();
     }
     
 
     public NewPanel2() {
         this.addMouseListener(this);
-        timer = new Timer (5,this);
+        timer = new Timer (40,this);
         timer.start();
     }
     
     @Override
     protected void paintComponent(Graphics g){
-        super.paintComponent(g);
+        super.paintComponent(g);  
+        
+        Image fondo = loadImage("fondo.png");
+        g.drawImage(fondo, 0, 0,null);
+        
+        Image Persona = loadImage("free_radical_game_sprites.png");
+     
+        g.drawImage(Persona,x,80,x+32,80+32,(this.secuencia*32)+32,0,32,32,this);
+        
+     
+        Image moto = loadImage("scooter.gif");
+       
+        g.drawImage(moto,380,380,null);
+//        Image gato = loadImage("cats.gif");
+//        g.drawImage(gato,x,380,132+x,460,(this.secuencia*132),0,(this.secuencia*132)+132,80,this);
+//        
+        g.setColor(Color.ORANGE);
+        
+        g.fillRect(x,380,50 , 50);
+        g.setColor(Color.GREEN);
+        g.fillRect(x+40,380,60,12);
+        g.setColor(Color.MAGENTA);
+        g.fillOval(x+10, 430, 20, 20);
+        g.fillOval(x+40, 430, 20, 20);
+        
+        g.setColor(Color.DARK_GRAY);
+        g.drawRect(x,380,100,70);
         
         g.setColor(Color.ORANGE);
         
-        g.drawRect(x,450,50 , 50);
+        g.drawRect(x+200,400,50 , 50);
         g.setColor(Color.GREEN);
-        g.drawRect(x+40,460,60,12);
+        g.drawRect(x+40+200,400,60,12);
         g.setColor(Color.MAGENTA);
-        g.drawOval(x+10, 500, 20, 20);
-        g.drawOval(x+40, 500, 20, 20);
+        g.drawOval(x+10+200, 450, 20, 20);
+        g.drawOval(x+40+200, 450,20, 20);
         
         g.setColor(Color.DARK_GRAY);
-        g.drawRect(x,450,100,70);
-        
-        g.setColor(Color.ORANGE);
-        
-        g.drawRect(x+200,450,50 , 50);
-        g.setColor(Color.GREEN);
-        g.drawRect(x+40+200,460,60,12);
-        g.setColor(Color.MAGENTA);
-        g.drawOval(x+10+200, 500, 20, 20);
-        g.drawOval(x+40+200, 500, 20, 20);
-        
-        g.setColor(Color.DARK_GRAY);
-        g.drawRect(x+200,450,100,70);
+        g.drawRect(x+200,400,100,70);
         
         
         g.setColor(Color.red);
@@ -115,7 +171,17 @@ class NewPanel2 extends JPanel implements ActionListener,MouseListener{
     public Rectangle getBounds(){
         return new Rectangle(x,450,100,70);
     }
-    
+   
+    public boolean checkCollisions(){
+        Rectangle tanque = getBounds();
+        Rectangle objeto = new Rectangle(200,450,100,70);
+        if(tanque.intersects(objeto)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     @Override
     public void mouseClicked(MouseEvent e) {
         System.out.println("Soy el puto amo");
